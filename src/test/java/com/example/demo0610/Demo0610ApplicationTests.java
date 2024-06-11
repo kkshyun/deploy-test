8aec7faebcb02f7432940e89238731f5b6864af6
Kim Sehyun <kkshyun56@gmail.com>
2024-06-11 10:00:06 +0900

    first commit

---
+++ src/test/java/com/example/demo0610/Demo0610ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo0610;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo0610ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
